984b5cbc6d30d899f537888efa6e2af405402626
DoubleZPlaying <zevieywonder@gmail.com>
2022-04-04 22:37:06 -0700

    Test getAttack()

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main (String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        Player player1 = new Player(10, 3, 4, 5);
+    }
+}
