311d7ed9e53b1d22595a997e564b8ef653302aa5
DoubleZPlaying <zevieywonder@gmail.com>
2022-04-08 00:06:50 -0700

    work on why exception when killing monster

--- Battle.java
+++ Battle.java
@@ -59,6 +59,61 @@ public class Battle
         return "You find yourself in a room made of packed dirt. Your head is sprinkled with dust from the low ceiling where you can hear insects crawling. A lantern on the floor aluminates the entire room except for the shadowy corners. ";
     }
 
+    public static void hallWay()
+    {
+        int hall = (int)(Math.random() * (4 + 1) + 1);
+
+        if(hall == 1)
+        {
+            Battle.hall1();
+        }
+
+        else if(hall == 2)
+        {
+            Battle.hall2();
+        }
+
+        else if(hall == 3)
+        {
+            Battle.hall3();
+        }
+
+        else if(hall == 4)
+        {
+            Battle.hall4();
+        }
+
+        else if(hall == 5)
+        {
+            Battle.hall5();
+        }
+    }
+
+    public static String hall1()
+    {
+        return "You walk through a short hallway made entirely of stone bricks. At the end of the hall is a heavy, wooden door with an iron ring as its handle. You pull it open and step inside. ";
+    }
+
+    public static String hall2()
+    {
+        return "You walk through a slightly ovular hallway of roughly hewn stone. Already crouching beneath the low ceiling, you crouch lower to fit through the door, which is the size of a trapdoor. You squeeze through and stand up. ";
+    }
+
+    public static String hall3()
+    {
+        return "You climb up a steep staircase made of creaky cheery wood. At the top of the staircase is a rope ladder hung from a hole in the ceiling. Climbing up, you pull yourself into the next room. ";
+    }
+
+    public static String hall4()
+    {
+        return "Before you lies a massive crevice. You peer down into it, but don't see a bottom, only shadow. You jump across the crevice onto the narrow platform in front of the door and step through.";
+    }
+
+    public static String hall5()
+    {
+        return "You make your way through a stone passageway covered in slipper moss. The air is so dank and moist you can feel the water vapor settling in your chest. You hurriedly barge through the half-rotted wooden door at the end of the passage.";
+    }
+
     public static String monster()
     {
         int monster = (int)(Math.random() * (5 + 1) + 1);
